9bd6c09cee90aad9cdc4214bf1963be8227086cc
Ahmed Hassan <dr.riosx@gmaio.com>
2025-12-02 20:48:55 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Change 'portfolio' to your repository name
+  base: '/', // Change 'portfolio' to your repository name
 })
